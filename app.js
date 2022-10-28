e68f258762cecd453a0210f0ec38a6ee825c7600
Nikhil George <nikhilgeorge2015@gmail.com>
2022-10-28 23:15:32 +0530

    feat: custom percentages

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const percentageSelect = document.getElementById("percentage");
 const presentInput = document.getElementById("present-input");
 const totalInput = document.getElementById("total-input");
 const btn = document.getElementById("btn");
@@ -6,25 +7,29 @@ const outputDiv = document.getElementById("output-div");
 btn.addEventListener("click", () => {
   let present = Number(presentInput.value);
   let total = Number(totalInput.value);
+  let percentage = Number(percentageSelect.value);
+
   if (present <= 0 || total <= 0 || present > total) {
     return (outputDiv.innerText = "Proper values please ¯\\_(ツ)_/¯");
   }
-  if (present / total >= 0.75) {
-    const daysAvailableToBunk = daysToBunk(present, total);
+
+  if (present / total >= percentage / 100) {
+    const daysAvailableToBunk = daysToBunk(present, total, percentage);
     return (outputDiv.innerHTML = `You can bunk for <strong>${daysAvailableToBunk}</strong> more days :)<br>Total Attendance will be then(P/T):<strong>${present}/${
       total + daysAvailableToBunk
     }</strong>`);
   }
-  const attendanceNeeded = reqAttendance(present, total);
-  return (outputDiv.innerHTML = `You need to attend <strong>${attendanceNeeded}</strong> more days to have 75% attendance<br>Total Attendance Required(P/T): <strong>${
+
+  const attendanceNeeded = reqAttendance(present, total, percentage);
+  return (outputDiv.innerHTML = `You need to attend <strong>${attendanceNeeded}</strong> more days to have ${percentage}% attendance<br>Total Attendance Required(P/T): <strong>${
     attendanceNeeded + present
   }/${attendanceNeeded + total}</strong>`);
 });
 
-const reqAttendance = (present, total) => {
-  return 3 * total - 4 * present;
+const reqAttendance = (present, total, percentage) => {
+  return Math.floor((percentage * total - 100 * present) / (100 - percentage));
 };
 
-const daysToBunk = (present, total) => {
-  return Math.floor((4 * present - 3 * total) / 3);
+const daysToBunk = (present, total, percentage) => {
+  return Math.floor((100 * present - percentage * total) / percentage);
 };
